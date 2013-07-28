1e77b96627ea21211c669bbeb1ba7cb36136cb09
Sergey Gerasimov <mail@grsmv.com>
2013-07-28 15:25:32 +0300

    Request parsing added

--- server/src/Server.hs
+++ server/src/Server.hs
@@ -7,38 +7,85 @@ import System.Directory (getCurrentDirectory)
 import System.IO
 import System.Posix.Process (getProcessID)
 
+
+{------------------------------------------------------------------------------
+                                   Settings
+------------------------------------------------------------------------------}
+
+data Setting = Setting { k :: String, v :: String } deriving Show
+
+
+settings :: [Setting]
+settings = [ Setting "port" "5002"
+           , Setting "tmp"  "/Users/sergey/Desktop/" ]
+
+
+-- | Searching for a specific key through list of settings
+settingValue :: String -- ^ key
+             -> String -- ^ value
+settingValue key = v . head $ filter (\s -> k s == key) settings
+
+{------------------------------------------------------------------------------
+                                    Server
+------------------------------------------------------------------------------}
+
+
 data RequestType = GET | POST deriving Show
-data Request = Request { rtype :: RequestType
-                       , path :: String }
+data Request = Request { rtype :: RequestType , path :: String }
 
 instance Show Request where
-    show r = "Request {" ++ show (rtype r) ++ " " ++ path r ++ "\n}"
+    show r = "Request {" ++ show (rtype r) ++ " " ++ path r ++ "}"
+
 
 start :: IO ()
 start = withSocketsDo $ do
-         sock <- listenOn $ PortNumber 5002
-         forever $ do
-           (handle, _, _) <- accept sock
-           {- parseRequest handle -}
-           forkIO $ do
-              hPutStr handle msg
-              hFlush handle
-              hClose handle
+    let port = read (settingValue "port") :: Integer
+    sock <- listenOn $ PortNumber (fromInteger port)
+    forever $ do
+      (handle, _, _) <- accept sock
+
+      -- getting request details
+      request <- hGetContents handle
+      print $ parseRequest request
+
+      forkIO $ do
+         hPutStr handle index
+         hFlush handle
+         hClose handle
 
 
-parseRequest :: Handle -> IO ()
-parseRequest handle = do
-    content <- hGetContents handle
-    putStrLn content
+{------------------------------------------------------------------------------
+                               Server helpers
+------------------------------------------------------------------------------}
+
+-- | Parsing incoming request
+parseRequest :: String  -- ^ raw string with request headers
+             -> Request -- ^ parsed request details
+parseRequest requestHeaders =
+    case words (head (lines requestHeaders)) of
+      [t, p, _] -> Request { rtype=fromString t, path=p }
 
 
-msg :: String
-msg = "HTTP/1.0 200 OK\r\nContent-Length: 6\r\n\r\nSllar!\r\n"
+-- | Converting string presentation of request type into specific type
+fromString :: String      -- ^ string presentation of request type
+           -> RequestType -- ^ value of a specific type
+fromString t = case t of
+                 "GET" -> GET
+                 "POST" -> POST
 
 
+-- | Memoizing Process PID, recording it to a pid file
 writePid :: IO ()
 writePid = do
     pid <- getProcessID
     pwd <- getCurrentDirectory
     let pidStr = show pid :: String
     writeFile (pwd ++ "/server.pid") pidStr
+
+
+{------------------------------------------------------------------------------
+                               Specific routes
+------------------------------------------------------------------------------}
+
+index :: String
+index = "HTTP/1.0 200 OK\r\nContent-Length: 6\r\n\r\nSllar!\r\n"
