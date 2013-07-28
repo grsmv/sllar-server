e85d914f8d95827b801034b9cb098c5935bb58c5
Sergey Gerasimov <mail@grsmv.com>
2013-07-28 14:07:15 +0300

    main.hs -> server.hs

--- server/src/main.hs
+++ server/src/main.hs
@@ -1,44 +1,21 @@
 module Main where
 
-import Control.Concurrent
-import Control.Monad (forever)
-import Network
-import System.Directory (getCurrentDirectory)
-import System.IO
-import System.Posix.Process (getProcessID)
-
-data RequestType = GET | POST deriving Show
-data Request = Request { rtype :: RequestType
-                       , path :: String }
-
-instance Show Request where
-    show r = "Request {" ++ show (rtype r) ++ " " ++ path r ++ "\n}"
+import qualified Server
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
 
 main :: IO ()
-main = withSocketsDo $ do
-         sock <- listenOn $ PortNumber 5002
-         forever $ do
-           (handle, _, _) <- accept sock
-           parseRequest handle
-           forkIO $ do
-              hPutStr handle msg
-              hFlush handle
-              hClose handle
+main = do
+    args <- getArgs
+    if null args then argFailure
+                 else case head args of
+                        "server"   -> Server.start
+                        "stop"     -> putStrLn "stop server"
+                        "update"   -> putStrLn "update info"
 
 
-parseRequest :: Handle -> IO ()
-parseRequest handle = do
-    content <- hGetContents handle
-    putStrLn content
-
-
-msg :: String
-msg = "HTTP/1.0 200 OK\r\nContent-Length: 6\r\n\r\nSllar!\r\n"
-
-
-writePid :: IO ()
-writePid = do
-    pid <- getProcessID
-    pwd <- getCurrentDirectory
-    let pidStr = show pid :: String
-    writeFile (pwd ++ "/server.pid") pidStr
+-- Raising exit with message if number of arguments is incorrect
+argFailure :: IO ()
+argFailure = do
+    putStrLn "Specify argument"
+    exitFailure
---
+++ server/src/server.hs
@@ -0,0 +1,44 @@
+module Server (start) where
+
+import Control.Concurrent
+import Control.Monad (forever)
+import Network
+import System.Directory (getCurrentDirectory)
+import System.IO
+import System.Posix.Process (getProcessID)
+
+data RequestType = GET | POST deriving Show
+data Request = Request { rtype :: RequestType
+                       , path :: String }
+
+instance Show Request where
+    show r = "Request {" ++ show (rtype r) ++ " " ++ path r ++ "\n}"
+
+start :: IO ()
+start = withSocketsDo $ do
+         sock <- listenOn $ PortNumber 5002
+         forever $ do
+           (handle, _, _) <- accept sock
+           parseRequest handle
+           forkIO $ do
+              hPutStr handle msg
+              hFlush handle
+              hClose handle
+
+
+parseRequest :: Handle -> IO ()
+parseRequest handle = do
+    content <- hGetContents handle
+    putStrLn content
+
+
+msg :: String
+msg = "HTTP/1.0 200 OK\r\nContent-Length: 6\r\n\r\nSllar!\r\n"
+
+
+writePid :: IO ()
+writePid = do
+    pid <- getProcessID
+    pwd <- getCurrentDirectory
+    let pidStr = show pid :: String
+    writeFile (pwd ++ "/server.pid") pidStr
