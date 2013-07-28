f1e3b55b573b8f659b64fec82f039e5abb659099
Sergey Gerasimov <mail@grsmv.com>
2013-07-28 23:25:43 +0300

    Client: code simplification

--- client/src/Main.hs
+++ client/src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Exception (onException)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 
@@ -7,36 +8,27 @@ import System.Exit (exitFailure)
 main :: IO ()
 main = do
     args <- getArgs
-    if null args then argFailure
-                 else case head args of
-                        "list"    -> p "list of packages"
-                        "update"  -> p "update list of packages from Sllar repository"
-                        "publish" -> p "send package to a Sllar repository"
-                        "help"    -> p "help"
-                        "env"     -> p "show your current configuration"
-                        "install" -> do package <- packageName args
-                                        p $ "install package " ++ package
-                        "show"    -> do package <- packageName args
-                                        p $ "show information about package " ++ package
-                        "create"  -> do package <- packageName args
-                                        p $ "create a skeleton of a package with name " ++ package
-                        _ -> argFailure
+    if null args
+      then do
+        p "Specify argument"
+        exitFailure
+      else case head args of
+             "list"    -> p "list of packages"
+             "update"  -> p "update list of packages from Sllar repository"
+             "publish" -> p "send package to a Sllar repository"
+             "help"    -> p "help"
+             "env"     -> p "show your current configuration"
+             "install" -> withName $ p ("install package " ++ args !! 1)
+             "show"    -> withName $ p ("show information about package " ++ args !! 1)
+             "create"  -> withName $ p ("create a skeleton of a package with name " ++ args !! 1)
+             _         -> p "help"
+    where p = putStrLn
 
 
--- Getting package name from list of arguments
-packageName :: [String] -- List of arguments
-            -> IO String
-packageName args = if length args > 1 then return (args !! 1)
-                                      else do p "Specify package name"
-                                              exitFailure
-
--- Raising exit with message if number of arguments is incorrect
-argFailure :: IO ()
-argFailure = do
-    p "Specify argument"
-    exitFailure
-
-
--- Shortcut for `putStrLn`
-p :: String -> IO ()
-p = putStrLn
+-- | Wrapper around actions with package name.
+-- Stops application execution if package name not given
+withName :: IO a -> IO a
+withName action =
+    onException action
+      (do print "Specify package name"
+          exitFailure)
