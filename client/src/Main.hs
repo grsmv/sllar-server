9336185f9f0fbd060feccef3df1774cad8b16c45
Sergey Gerasimov <mail@grsmv.com>
2013-07-30 20:33:24 +0300

    Client: conditions added to Main

--- client/src/Main.hs
+++ client/src/Main.hs
@@ -1,29 +1,35 @@
 module Main where
 
-import Control.Exception (onException)
+import Examination (checkEnv)
+import Control.Exception (onException, finally)
+import Control.Monad (when)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 
 
 main :: IO ()
-main = do
-    args <- getArgs
-    if null args
-      then do
-        p "Specify argument"
-        exitFailure
-      else case head args of
-             "setup"   -> p "setup your environment"
-             "list"    -> p "list of packages"
-             "update"  -> p "update list of packages from Sllar repository"
-             "publish" -> p "send package to a Sllar repository"
-             "help"    -> p "help"
-             "env"     -> p "show your current configuration"
-             "install" -> withName $ p ("install package " ++ args !! 1)
-             "show"    -> withName $ p ("show information about package " ++ args !! 1)
-             "create"  -> withName $ p ("create a skeleton of a package with name " ++ args !! 1)
-             _         -> p "help"
-    where p = putStrLn
+main =
+    withArguments $
+      do args <- getArgs
+         let p = putStrLn
+         case head args of
+           "setup"   -> p "setup your environment"
+           "list"    -> p "list of packages"
+           "update"  -> p "update list of packages from Sllar repository"
+           "publish" -> p "send package to a Sllar repository"
+           "help"    -> p "help"
+           "env"     -> p "show your current configuration"
+           "install" -> withName $ p ("install package " ++ args !! 1)
+           "show"    -> withName $ p ("show information about package " ++ args !! 1)
+           "create"  -> withName $ p ("create a skeleton of a package with name " ++ args !! 1)
+           _         -> p "help"
+
+
+withArguments :: IO () -> IO ()
+withArguments action =
+    finally (do args <- getArgs
+                when (null args) $ putStrLn "specify argument")
+            (onException action exitFailure)
 
 
 -- | Wrapper around actions with package name.
