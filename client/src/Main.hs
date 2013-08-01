713db5fd8a6717158d8c9cd309f4f87744033614
Sergey Gerasimov <mail@grsmv.com>
2013-08-01 21:19:41 +0300

    Client: refactoring in main

--- client/src/Main.hs
+++ client/src/Main.hs
@@ -9,23 +9,22 @@ import System.Environment (getArgs)
 
 main :: IO ()
 main =
-  do env <- checkEnv "TMT_HOME"
+  do env <- checkEnv "GOPATH"
      when env $
        do args <- getArgs
-          if null args
-            then failDown "Specify an argument(s)"
-            else do let p = putStrLn
-                    case head args of
-                      "setup"   -> p "setup your environment"
-                      "list"    -> p "list of packages"
-                      "update"  -> p "update list of packages from Sllar repository"
-                      "publish" -> p "send package to a Sllar repository"
-                      "help"    -> p "help"
-                      "env"     -> p "show your current configuration"
-                      "install" -> withName $ p ("install package " ++ args !! 1)
-                      "show"    -> withName $ p ("show information about package " ++ args !! 1)
-                      "create"  -> withName $ p ("create a skeleton of a package with name " ++ args !! 1)
-                      _         -> p "help"
+          withArgs args $
+            do let p = putStrLn
+               case head args of
+                 "setup"   -> p "setup your environment"
+                 "list"    -> p "list of packages"
+                 "update"  -> p "update list of packages from Sllar repository"
+                 "publish" -> p "send package to a Sllar repository"
+                 "help"    -> p "help"
+                 "env"     -> p "show your current configuration"
+                 "install" -> withName $ p ("install package " ++ args !! 1)
+                 "show"    -> withName $ p ("show information about package " ++ args !! 1)
+                 "create"  -> withName $ p ("create a skeleton of a package with name " ++ args !! 1)
+                 _         -> p "help"
 
 --
 -- Wrapper around actions with package name.
@@ -37,3 +36,14 @@ withName :: IO a -> IO a
 withName action =
     onException action
       (failDown "Specify package name")
+
+
+--
+-- Wrapper, that checks if any argument specified
+-- Input: arguments, function to applicationy if arguments exists
+-- Output: wrapped function
+--
+withArgs :: [String] -> IO () -> IO ()
+withArgs args f = if null args
+                    then failDown "No arguments specified"
+                    else f
