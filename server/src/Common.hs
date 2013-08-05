12b6f9c5f3f91037e7924b8fe81b588fe80bf4ae
Sergey Gerasimov <mail@grsmv.com>
2013-08-05 13:34:51 +0300

    Client, server: helpers fol colorizing output text

--- server/src/Common.hs
+++ server/src/Common.hs
@@ -1,6 +1,11 @@
-module Common (failDown) where
+module Common
+  ( failDown
+  , red
+  , green
+  , yellow ) where
 
 import System.Exit (exitFailure)
+import System.Console.ANSI
 
 --
 -- Graceful dying with message
@@ -9,3 +14,27 @@ import System.Exit (exitFailure)
 failDown :: String -> IO ()
 failDown s = do putStrLn ("Error: " ++ s)
                 exitFailure
+
+
+--
+-- Generic color-printing helper
+-- Input: text to colorize, Color to colorize in
+-- Output: IO action
+--
+color :: String -> Color -> IO ()
+color msg color' = do
+    setSGR [ SetConsoleIntensity BoldIntensity
+           , SetColor Foreground Vivid color' ]
+    putStr msg
+    setSGR [Reset]
+
+
+--
+-- Bunch of public colorizing helpers
+-- Input: text to colorize
+-- Output: IO action
+--
+red, green, yellow :: String -> IO ()
+red msg = color msg Red
+green msg = color msg Green
+yellow msg = color msg Yellow
