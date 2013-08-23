0d4998c1d276f52555f1a8d7896fdb7355629528
Sergey Gerasimov <mail@grsmv.com>
2013-08-23 21:55:47 +0300

    S: issue #12 - versions moved to 'versions' table, 'uploaded_at' added to versions

--- server/src/Database.hs
+++ server/src/Database.hs
@@ -1,6 +1,8 @@
 -- Bridge to SQLite database, which is used for storing
 -- information about packages. Also can create new database.
+--     http://www.sqlite.org/foreignkeys.html
 -- NB: `PRAGMA foreign_keys = ON` for enabling foreign keys support
+-- TODO: add `uploadDate` for packages and versions
 
 module Database (create) where
 
@@ -14,23 +16,24 @@ import qualified Paths_sllar_server as Paths
 -- Output: list of tables with descriptions
 --
 dataTables :: [(String, [(String, SQLType, [Clause])])]
-dataTables = [ ("packages",
-               --  column name     column type                additional properties
-                 [ ("id",          SQLInt NORMAL False False, [PrimaryKey True, Unique])
-                 , ("name",        SQLVarChar 100,            m)
-                 , ("description", SQLVarChar 1000,           m)
-                 , ("author",      SQLVarChar 100,            m)
-                 , ("version",     SQLVarChar 10,             m)
-                 , ("maintainer",  SQLVarChar 100,            o)
-                 , ("license",     SQLVarChar 100,            o)
-                 , ("copyright",   SQLVarChar 100,            o)
-                 , ("homepage",    SQLVarChar 100,            o)
-                 , ("tracker",     SQLVarChar 100,            o)])
-             , ("versions",
-                 [ ("id",          SQLInt NORMAL False False, [PrimaryKey True, Unique])
-                 , ("packageId",   SQLInt NORMAL False False, [ForeignKey "packages" ["id"] [OnUpdate Cascade, OnDelete Cascade] Nothing])
-                 , ("version",     SQLVarChar 10,             m)])]
-             where (m, o) = ([IsNullable False], [IsNullable True])
+dataTables =
+    [ ("packages",
+        --  column name     column type                additional properties
+          [ ("id",          SQLInt NORMAL False False, [PrimaryKey True, Unique])
+          , ("name",        SQLVarChar 100,            m)
+          , ("description", SQLVarChar 1000,           m)
+          , ("author",      SQLVarChar 100,            m)
+          , ("maintainer",  SQLVarChar 100,            o)
+          , ("license",     SQLVarChar 100,            o)
+          , ("copyright",   SQLVarChar 100,            o)
+          , ("homepage",    SQLVarChar 100,            o)
+          , ("tracker",     SQLVarChar 100,            o)])
+      , ("versions",
+          [ ("id",          SQLInt NORMAL False False, [PrimaryKey True, Unique])
+          , ("package_id",  SQLInt NORMAL False False, [ForeignKey "packages" ["id"] [OnUpdate Cascade, OnDelete Cascade] Nothing])
+          , ("version",     SQLVarChar 10,             m)
+          , ("uploaded_at", SQLDateTime DATETIME,      m)])]
+      where (m, o) = ([IsNullable False], [IsNullable True])
 
 
 --
