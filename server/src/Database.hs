c4a915003aaa9d91718d47284d8af97822e47a42
Sergey Gerasimov <mail@grsmv.com>
2013-08-27 14:43:36 +0300

    s: issue #12, 'createPackage' updated

--- server/src/Database.hs
+++ server/src/Database.hs
@@ -16,8 +16,10 @@ module Database
 import qualified Package
 
 -- system
+import Data.Maybe (fromMaybe)
 import Control.Exception (bracket)
 import Database.SQLite
+import Data.DateTime (getCurrentTime)
 import qualified Paths_sllar_server as Paths
 
 
@@ -77,11 +79,32 @@ create =
 -- Creating package in the Database
 --
 createPackage :: Package.Package -> IO ()
-createPackage pkg = withConnection $ \handle -> do
-  _ <- insertRow handle "packages" (Package.toTuple pkg)
-  return ()
+createPackage pkg = withConnection $ \h -> do
 
-  -- split package data to Package info and version info
-  -- check if package already presented with lower version
-  -- if presented - update version
-  -- if not - record Package info and version info
+    currentDateTime <- getCurrentTime
+
+    let packageInfo = Package.toTuple pkg
+        createVersion packageId =
+          insertRow h "versions" [
+              ("version",     fromMaybe "" $ lookup "version" packageInfo),
+              ("package_id",  show packageId),
+              ("uploaded_at", show currentDateTime)]
+
+    ls <- execStatement h $ "select id from packages where name='" ++ Package.name pkg ++ "'"
+
+    case ls :: Either String [[Row Value]] of
+        Right [row] ->
+
+            -- check if package already presented with lower version
+            case length row of
+
+                -- if not exists - create package and version
+                0 -> do insertRow h "packages" (filter (\(k, _) -> k /= "version") packageInfo)
+                        rowId <- getLastRowID h
+                        createVersion rowId
+
+                -- otherwise - create version
+                _ -> do let Just (Int id') = lookup "id" $ head row
+                        createVersion id'
+
+    return ()
