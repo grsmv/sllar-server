8b1af34f0ce82bfe23ebb89d33694c9824721548
Sergey Gerasimov <mail@grsmv.com>
2013-10-26 19:10:44 +0300

    s: PackageSpec updated

--- server/spec/PackageSpec.hs
+++ server/spec/PackageSpec.hs
@@ -11,27 +11,36 @@ main = hspec $ do
 
     describe "Package.info" $ do
         it "should present Sllar package information in system-readable format if all mandatory fields filled" $ do
-            package <- BS.readFile "../fixtures/example.yaml" >>= info
-            package `shouldBe` Just Package { name="package", description="example description",
-                                              author="me", version="0.0.1", maintainer=Nothing,
-                                              license=Nothing, copyright=Nothing, tracker=Nothing,
-                                              homepage=Nothing }
+            package <- BS.readFile "fixtures/example.yaml" >>= info
+            package `shouldBe` Just Package { name = "package"
+                                            , description = "example description"
+                                            , author = "me"
+                                            , version = "0.0.1"
+                                            , maintainer = Nothing
+                                            , license = Nothing
+                                            , copyright = Nothing
+                                            , tracker = Nothing
+                                            , homepage = Nothing
+                                            }
 
         it "should return Nothing if one of mandatory fields not filled" $ do
-            package <- BS.readFile "../fixtures/wrong_example.yaml" >>= info
+            package <- BS.readFile "fixtures/wrong_example.yaml" >>= info
             package `shouldBe` Nothing
 
 
+    -- todo: remove this after closing issue #15
     describe "Package.defaultFields" $
         it "should return fields of Package type" $
             defaultFields `shouldBe` ["name", "description", "author", "version", "maintainer", "license", "copyright", "homepage", "tracker"]
 
 
+    -- todo: remove this after closing issue #15
     describe "Package.unusedFields" $
         it "should return fields, that not used in Sllar file" $
             unusedFields exampleSllarFileContents defaultFields `shouldBe` ["maintainer", "license", "copyright", "homepage", "tracker"]
 
 
+    -- todo: remove this after closing issue #15
     describe "Package.correct" $
         it "should add fields, that wasn't pesent in Sllar file, uploaded from client" $
             correct exampleSllarFileContents `shouldBe` exampleSllarFileContents ++ "maintainer: \nlicense: \ncopyright: \nhomepage: \ntracker: \n"
@@ -39,7 +48,7 @@ main = hspec $ do
 
     describe "Package.toTuple" $
         it "should present Package as tuple" $ do
-            package <- BS.readFile "../fixtures/example.yaml" >>= info
+            package <- BS.readFile "fixtures/example.yaml" >>= info
             let Just pkg = package
             toTuple pkg `shouldBe` [("name", "package"), ("description", "example description"), ("author", "me"), ("version", "0.0.1"),
                                     ("maintainer", ""), ("license", ""), ("copyright", ""), ("homepage", ""), ("tracker", "")]
